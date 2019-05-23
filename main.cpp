912bd2edb8e9cfa16d86e256411f70785ea0fd54
takaiten <lopilopj@gmail.com>
2019-05-23 17:26:01 +0700

    Value calculation

--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@ using namespace Numerical_Integration;
 
 int main()
 {
-    
     auto f = [](double x)
     { return pow(x, 5) + 4 * pow(x, 3); };
     
@@ -19,25 +18,24 @@ int main()
     
     /* auto f = [](double x)
      { return 3 * pow(x, 8) + 2 * x; };
-  
+
      auto F = [](double x)
-     { return pow(x, 9) / 3 + pow(x, 2); };*/
+     { return pow(x, 9) / 3 + pow(x, 2); };
+
+    auto f = [](double x)
+    { return 5 * pow(x, 9) + 6 * pow(x, 5); };
+
+    auto F = [](double x)
+    { return pow(x, 10) / 2 + pow(x, 6); };
+
+    auto f = [](double x)
+    { return 11 * pow(x, 10) + 6 * pow(x, 2); };
+
+    auto F = [](double x)
+    { return pow(x, 11) + 2 * pow(x, 3); };*/
     
-    //auto f = [](double x)
-    //{ return 5 * pow(x, 9) + 6 * pow(x, 5); };
-    
-    //auto F = [](double x)
-    //{ return pow(x, 10) / 2 + pow(x, 6); };
-    
-    //auto f = [](double x)
-    //{ return 11 * pow(x, 10) + 6 * pow(x, 2); };
-    
-    //auto F = [](double x)
-    //{ return pow(x, 11) + 2 * pow(x, 3); };
-    
-    double a = 0,
-            b = 3;
-    int N = 10;
+    const double a = 0, b = 3;
+    const int n = 10;
     
     std::cout << "a = " << a
               << "\tb = " << b << std::endl << std::endl;
@@ -50,16 +48,15 @@ int main()
     {
         std::cout << "type: " << i << std::endl;
         
-        
         double PreviousIh = 0.0;
         double PreviousItrue = 0.0;
         
-        Integration_Scheme I((Integration_Scheme::Integration_Type) i);
+        Integration_Scheme I(static_cast<Integration_Scheme::Integration_Type>(i));
         for (int j = 1; j < 5; j *= 2)
         {
-            double I_calc = I.calculate(a, b, N * j, f);
-            double I_true = F(b) - F(a);
-            std::cout << std::endl << "H = " << fabs(b - a) / (N * j) << std::endl
+            const double I_calc = I.calculate(a, b, n * j, f);
+            const double I_true = F(b) - F(a);
+            std::cout << std::endl << "H = " << fabs(b - a) / (n * j) << std::endl
                       << "calculated: " << I_calc << std::endl
                       << "true: " << I_true << std::endl
                       << "diff: " << fabs(I_calc - I_true) << std::endl;
@@ -74,8 +71,6 @@ int main()
                           << log2((I_calc - PreviousIh) / (PreviousItrue - I_calc) + 1.0) << std::endl;
             PreviousIh = I_calc;
             PreviousItrue = I_true;
-            
-            
         }
         std::cout << std::endl << std::endl;
     }
